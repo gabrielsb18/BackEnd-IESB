cb5f613a6d546daa74e1a0fbb85fb857342eab6c
gabrielsb18 <gbmodelator@gmail.com>
2024-03-07 21:49:31 -0300

    Aula 04 concluida - Fazendo Testes

---
+++ aulas/Aula04/index.test.js
@@ -0,0 +1,9 @@
+const supertest = require("supertest");
+const app = require("./index");
+
+const request = supertest(app);
+
+test("Deve retonar 200 no GET", async function (){
+    const response = await request.get("/");
+    expect(response.status).toBe(200);
+})
